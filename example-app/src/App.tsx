d11ca7652fc03ad574c5b4f290c4529fae58b20f
Julian <tipler.julian@gmail.com>
2024-03-03 23:25:07 -0600

    made example app more robust

--- example-app/src/App.tsx
+++ example-app/src/App.tsx
@@ -4,7 +4,33 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>Example</>;
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          backgroundColor: "black",
+        }}
+      >
+        <h1 style={{ color: "white" }}>Hello, World!</h1>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          backgroundColor: "black",
+        }}
+      >
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+        <h1 style={{ color: "white" }}>{count}</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
