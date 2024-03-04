e64f6691ac4564064a3aae5080c2636e9bf9a304
Julian <tipler.julian@gmail.com>
2024-03-03 23:42:41 -0600

    removed commented iframe solution

--- chatbot/src/main.tsx
+++ chatbot/src/main.tsx
@@ -5,9 +5,9 @@ import "./index.css";
 
 const init = () => {
   const appDiv = document.createElement("div");
-  appDiv.id = "chatbot";
+  appDiv.id = "chatbot-app";
   document.body.appendChild(appDiv);
-  ReactDOM.createRoot(document.getElementById("chatbot")!).render(
+  ReactDOM.createRoot(document.getElementById("chatbot-app")!).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
@@ -15,54 +15,3 @@ const init = () => {
 };
 
 init();
-
-// function initChatbot() {
-//   const appDiv = document.createElement("div");
-//   appDiv.id = "chatbot-container";
-//   document.body.appendChild(appDiv);
-
-//   const iframe = document.createElement("iframe");
-//   iframe.style.width = "100%"; // Set width
-//   iframe.style.height = "400px"; // Set height, adjust as necessary
-//   iframe.style.border = "none"; // Remove border
-
-//   // Construct the HTML content for the iframe using a data URI
-//   const iframeContent = `
-//     <!DOCTYPE html>
-//     <html lang="en">
-//     <head>
-//       <meta charset="UTF-8">
-//       <title>Chatbot</title>
-//       <style>
-//         body { margin: 0; overflow: hidden; }
-//         /* Add more styles if necessary */
-//       </style>
-//     </head>
-//     <body>
-//       <div id="chatbot"></div>
-//       <script type="module" src="./chatbot.js"></script>
-//       <!-- The script src path must be accessible from the location of the iframe -->
-//     </body>
-//     </html>
-//   `;
-
-//   // Encode the HTML content and set it as the iframe's src using a data URI
-//   iframe.src = "data:text/html;charset=utf-8," + encodeURI(iframeContent);
-
-//   // Identify the target element where the chatbot iframe will be appended
-//   const targetElement = document.getElementById("chatbot-container"); // Ensure you have this element in your main app
-
-//   if (targetElement) {
-//     targetElement.appendChild(iframe);
-//   } else {
-//     console.error("Chatbot container element not found.");
-//   }
-// }
-
-// if (document.readyState === "loading") {
-//   // Loading hasn't finished yet
-//   document.addEventListener("DOMContentLoaded", initChatbot);
-// } else {
-//   // `DOMContentLoaded` has already fired
-//   initChatbot();
-// })
